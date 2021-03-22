aca798bbe08792ef6052813b47ffd7c2d06cce11
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2021-03-22 12:55:29 -0300

    ListaDuplamenteEncadeada

---
+++ ListaDinamicaDuplamenteEncadeada/LDDE.c
@@ -0,0 +1,173 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "LDDE.h"
+
+struct elemento
+{
+    struct elemento *ant;
+    struct aluno dados;
+    struct elemento *prox;
+};
+
+typedef struct elemento Elem;
+
+Lista* cria_lista(){
+    Lista* li =  (Lista*) malloc(sizeof(Lista));
+    if(li != NULL) *li = NULL;
+    return li;
+}
+void libera_lista(Lista* li){
+    if(li != NULL){
+        Elem* no;
+        while (*li != NULL)
+        {
+            no = *li;
+            *li = (*li)->prox;
+            free(no);
+        }
+        free(li);
+    }
+}
+int tamanho_lista(Lista* li){
+    if(li == NULL) return 0;
+    int count = 0;
+    Elem* no = *li;
+
+    while (no != NULL)
+    {
+        count++;
+        no = no->prox;
+    }
+
+    return count;
+}
+int lista_vazia(Lista* li){
+    if(li == NULL) return 1;
+    if(*li == NULL) return 1;
+    return 0;
+}
+int insere_lista_inicio(Lista* li, struct aluno al)
+{
+    if(li == NULL) return 0;
+    Elem* no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL) return 0;
+    no->dados = al;
+    no->prox = (*li);
+    no->ant = NULL;
+
+    if(*li != NULL)(*li)->ant = no;
+    *li = no;
+    return 1;
+};
+int insere_lista_final(Lista* li, struct aluno al){
+    if(li == NULL) return 0;
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL) return 0;
+    no->dados = al;
+    no->prox = NULL;
+    if((*li) == NULL){
+        no->ant = NULL;
+        *li = no;
+    }else{
+        Elem *aux = *li;
+        while(aux->prox != NULL) aux = aux->prox;
+        aux->prox = no;
+        no->ant = aux;
+    }
+    return 1;
+}
+int insere_lista_ordenada(Lista* li, struct aluno al){
+    if(li == NULL) return 0;
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL) return 0;
+    no->dados = al;
+    if(lista_vazia(li)){
+        no->prox = NULL;
+        no->ant = NULL;
+        *li = no;
+        return 1;
+    }else{
+        Elem *ante, *atual = *li;
+        while(atual != NULL && atual->dados.matricula < al.matricula){ 
+            ante = atual;
+            atual = atual->prox;
+        }
+        if(atual == *li){
+            no->ant = NULL;
+            (*li)->ant = no;
+            no->prox = (*li);
+            *li = no;
+        }else{
+            no->prox = ante->prox;
+            no->ant = ante;
+            ante->prox = no;
+            if(atual != NULL) atual->ant = no;
+        }
+        return 1;
+    }
+}
+int remove_lista_inicio(Lista* li){
+    if(lista_vazia(li)) return 0;
+    Elem *no = *li;
+    *li = no->prox;
+    if(no->prox != NULL) no->prox->ant = NULL;
+    no->prox = NULL;
+    free(no);
+}
+int remove_lista_final(Lista* li){
+    if(lista_vazia(li)) return 0;
+    Elem *no = *li;
+    while(no->prox != NULL) no = no->prox;
+    if(no->ant == NULL) *li = no->prox;
+    else no->ant->prox = NULL;
+    free(no);
+    return 1;
+}
+int remove_lista(Lista *li, int mat){
+    if(lista_vazia(li)) return 0;
+    Elem *no = *li;
+    while(no != NULL && no->dados.matricula != mat) no = no->prox;
+    if(no == NULL) return 0;
+    if(no->ant == NULL) *li = no->prox;
+    else no->ant->prox = no->prox;
+    if(no->prox != NULL) no->prox->ant = no->ant;
+    free(no);
+    return 1;
+}
+int consulta_lista_pos(Lista* li, int pos, struct aluno *al){
+    if(li == NULL || pos < 0) return 0;
+    Elem *no = *li;
+    int i = 0;
+    while( no != NULL && i < pos){
+        no = no->prox;
+        i++;
+    }
+    if(no == NULL) return 0;
+    else{
+        *al = no->dados;
+        return 1;
+    }
+}
+int consulta_lista_mat(Lista* li, int mat, struct aluno *al){
+    if(li == NULL) return 0;
+    Elem *no = *li;
+    while( no != NULL && no->dados.matricula != mat){
+        no = no->prox;
+    }
+    if(no == NULL) return 0;
+    else{
+        *al = no->dados;
+        return 1;
+    }
+}
+void imprime_Lista(Elem* no){
+    if(no ==  NULL){
+        return;
+    }
+    float nota = no->dados.n1 + no->dados.n2 + no->dados.n3; 
+    printf("aluno: %s matricula: %d nota: %.2f\n", no->dados.nome, no->dados.matricula, nota);
+
+    if(no->prox != NULL){
+        imprime_Lista(no->prox);
+    }
+}
