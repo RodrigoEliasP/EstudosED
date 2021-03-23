cecc224fa403dbc4465d96abe6bbf61841cfd06a
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2021-03-23 17:01:55 -0300

    Lista Dinamica Circular

---
+++ ListaCircular/main.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "ListaCircular.h"
+
+void cria_aluno(char nome[30], float n1,  float n2,  float n3, int mat, struct aluno* al){
+    strcpy(al->nome, nome);
+    al->n1 = n1;
+    al->n2 = n2;
+    al->n3 = n3;
+    al->matricula = mat;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    struct aluno al;
+    Lista* li = cria_lista();
+
+    cria_aluno("joao", 10, 10, 10, 1, &al);
+    insere_lista_inicio(li, al);
+    cria_aluno("marcos", 10, 10, 10, 3, &al);
+    insere_lista_final(li, al);
+    cria_aluno("jonas", 10, 10, 10, 2, &al);
+    insere_lista_ordenada(li, al);
+    imprime_lista(li);
+
+    remove_lista(li,2);
+    imprime_lista(li);
+    return 0;
+}
